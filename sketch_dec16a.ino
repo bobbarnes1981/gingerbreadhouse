06833e01251024be9ebc7ef59e715a9ccde43ac2
bobbarnes1981 <robertbarnes1981@googlemail.com>
2025-12-17 14:58:01 +0000

    Implement scroll myself

--- sketch_dec16a.ino
+++ sketch_dec16a.ino
@@ -2,13 +2,25 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+#define LCDADDR 0x27
 #define NUMPIXELS 67
+#define NUMCOLS 16
+#define NUMROWS 2
 
+#define TEXTDELAY 500
+#define LEDDELAY 250
+
+int textOffset = 0-NUMCOLS;
 int ledOffset = 0;
-char message[] = "Merry Christmas Everybody! Ho Ho Ho";
+
+char message[] = "Merry Christmas Everybody!";
+int length = 26;
 
 Adafruit_NeoPixel pixels(NUMPIXELS, 13, NEO_GRB);
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(LCDADDR, NUMCOLS, NUMROWS);
+
+unsigned long textElapsed = 0;
+unsigned long ledElapsed = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -21,10 +33,27 @@ void setup() {
 
   lcd.init();
   lcd.backlight();
-  lcd.print(message);
+  lcd.setCursor(0, 1);
+  lcd.print("* * * *  * * * *");
 }
 
-void loop() {
+void doText() {
+  lcd.setCursor(0, 0);
+  for (int i = 0; i < NUMCOLS; i++) {
+    if (i+textOffset >= length || i+textOffset < 0) {
+      lcd.print(' ');
+    } else {
+      lcd.print(message[i+textOffset]);
+    }
+  }
+  
+  textOffset++;
+  if (textOffset>length) {
+    textOffset = 0-NUMCOLS;
+  }
+}
+
+void doLed() {
   for (int i = 0; i < NUMPIXELS; i++) {
     switch ((ledOffset+i) % 3) {
       case 0:
@@ -44,8 +73,18 @@ void loop() {
   if (ledOffset>2) {
     ledOffset = 0;
   }
-  
-  lcd.scrollDisplayLeft();
+}
 
-  sleep(1);
+void loop() {
+  unsigned long m = millis();
+
+  if (m-textElapsed > TEXTDELAY) {
+    doText();
+    textElapsed = m;
+  }
+
+  if (m-ledElapsed > LEDDELAY) {
+    doLed();
+    ledElapsed = m;
+  }
 }
